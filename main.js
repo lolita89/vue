f0c04dd8cb31927617f908c3c122a7805feca601
lolita89 <cika.lolita@hotmail.com>
2018-06-18 16:02:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "https://photos-6.dropbox.com/t/2/AADfDjJtZtLH_jsJMFC5XKSH4aYFiLmuNaoBg3HjRkhcQw/12/703122049/jpeg/32x32/3/1529344800/0/2/vmSocks-green-onWhite.jpg/EJDIgOMGGKUJIAcoBw/8upE_sHYFbWfmd6CjMSrsyskgSg84nU-tJM2U0wZK8g?dl=0&size=1280x960&size_mode=3",
+        inventory: 11,
+        onSale: "On Sale!"
+    }
+})
